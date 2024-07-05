9c09c7ff146ae67b50ca352cc5f49d81451ec57f
qskkk <durand.romain.rd@gmail.com>
2024-07-05 11:34:38 +0200

    docs(test): api

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# backend-test
+# Backend Test en Go
+
+## Description
+
+This project is a backend test in Go. It aims to demonstrate backend development skills using the Go programming language. The application implements a simple REST API for managing resources.
+
+## Prerequisites
+
+Before you begin, ensure you have the following installed on your machine:
+
+- [Docker](https://www.docker.com/products/docker-desktop/)
+- [Git](https://git-scm.com/downloads)
+
+## Installation
+
+1. Clone the project repository
+2. Copy the `.env.example` file to `.env`
+3. Build the application `docker compose build`
+4. Run docker compose to start the application `docker compose up -d`
+
+## Tasks
+- Implement database migrations using the `database_actions` directory.
+- Implement the CRUD functionality for the breeds resource regarding the `breeds.csv` file.
+- Implement the search functionality to filter breeds based on pet characteristics (weight and species).
+
---
+++ main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"net"
+	"net/http"
+	"os"
+	"time"
+
+	charmLog "github.com/charmbracelet/log"
+	"github.com/gorilla/mux"
+	"github.com/japhy-tech/backend-test/database_actions"
+	"github.com/japhy-tech/backend-test/internal"
+)
+
+const (
+	MysqlDSN = "root:root@(mysql-test:3306)/core?parseTime=true"
+	ApiPort  = "5000"
+)
+
+func main() {
+	logger := charmLog.NewWithOptions(os.Stderr, charmLog.Options{
+		Formatter:       charmLog.TextFormatter,
+		ReportCaller:    true,
+		ReportTimestamp: true,
+		TimeFormat:      time.Kitchen,
+		Prefix:          "🧑‍💻 backend-test",
+		Level:           charmLog.DebugLevel,
+	})
+
+	err := database_actions.InitMigrator(MysqlDSN)
+	if err != nil {
+		logger.Fatal(err.Error())
+	}
+
+	msg, err := database_actions.RunMigrate("up", 0)
+	if err != nil {
+		logger.Error(err.Error())
+	} else {
+		logger.Info(msg)
+	}
+
+	db, err := sql.Open("mysql", MysqlDSN)
+	if err != nil {
+		logger.Fatal(err.Error())
+		os.Exit(1)
+	}
+	defer db.Close()
+	db.SetMaxIdleConns(0)
+
+	err = db.Ping()
+	if err != nil {
+		logger.Fatal(err.Error())
+		os.Exit(1)
+	}
+
+	logger.Info("Database connected")
+
+	app := internal.NewApp(logger)
+
+	r := mux.NewRouter()
+	app.RegisterRoutes(r.PathPrefix("/v1").Subrouter())
+
+	r.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}).Methods(http.MethodGet)
+
+	err = http.ListenAndServe(
+		net.JoinHostPort("", ApiPort),
+		r,
+	)
+
+	// =============================== Starting Msg ===============================
+	logger.Info(fmt.Sprintf("Service started and listen on port %s", ApiPort))
+}
