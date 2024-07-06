5ebc5228cad9469a1743025eb0b7a4b1d87cb2b1
Haroutioun Achtchian <harou@harou.home>
2024-07-06 11:53:40 +0200

    Created a new table for Breeds in the database to handle migration up and down

---
+++ database_actions/migrations/2_create_breeds_table.up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE breeds (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  species VARCHAR(255) NOT NULL,
+  pet_size VARCHAR(255) NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  average_male_adult_weight INT,
+  average_female_adult_weight INT
+);
+
